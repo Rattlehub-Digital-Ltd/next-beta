f625b2f13eb99bd09a302b9978477e1b3cbf07cc
Themba <mndebele.themba@gmail.com>
2025-07-31 22:04:13 +0200

    up

--- src/features/shared/user-badge.tsx
+++ src/features/shared/user-badge.tsx
@@ -10,7 +10,7 @@ export default function UserBadge({ name }: UserBadgeProps) {
 		<div className="flex">
 			<div className="bg-[#49454F]/8 pl-1 pr-2.5 items-center rounded-full py-[3px] flex space-x-1">
 				<div className="text-[#6750A4]">
-					<Icon icon="fluent:person-circle-20-filled" height={16} width={16} />
+					<Icon icon="fluent:person-circle-20-filled" height={18} width={18} />
 				</div>
 				<p className="text-[#49454F] text-xs tracking-[0.01px] font-medium">
 					{name}
