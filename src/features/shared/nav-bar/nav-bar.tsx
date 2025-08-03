fb1f9f94140ee6c3b8e137ed5dd827d0d326029e
Themba <mndebele.themba@gmail.com>
2025-08-03 20:35:45 +0200

    uip

--- src/features/shared/nav-bar/nav-bar.tsx
+++ src/features/shared/nav-bar/nav-bar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
 								getInitials(user?.name ?? user?.nickname ?? "")
 							) : (
 								<div className="text-white">
-									<Icon icon="fluent:person-20-filled" className="!w-4 !h-4" />
+									<Icon icon="fluent:person-20-filled" className="!w-5 !h-5" />
 								</div>
 							)}
 						</AvatarFallback>
