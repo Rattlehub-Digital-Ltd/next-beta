905cc59ff959e23ba28c6347dea0241ce4a9b82d
Themba <mndebele.themba@gmail.com>
2025-08-08 01:08:01 +0200

    up

--- src/features/dashboard/banner.tsx
+++ src/features/dashboard/banner.tsx
@@ -14,12 +14,12 @@ export default function Banner() {
 		<div className="h-10 flex items-center w-full bg-[#6155F5] text-white pl-4 pr-2 space-x-2  rounded-[16px]">
 			<SparkleIcon className="h-4 w-4 shrink-0" />
 			<p className="font-medium grow truncate text-[12px] tracking-wide">
-				Trial ending in{" "}
+				Your Trial ends in{" "}
 				<span className="text-yellow-300 font-bold">10 days</span>
 			</p>
 			<StripeDrawer>
 				<Button
-					className="text-xs gap-1 font-bold bg-white/95 backdrop-blur-md rounded-[10px] text-[#6155F5] h-6"
+					className="text-[11px] uppercase tracking-wide gap-1 font-bold bg-white/95 backdrop-blur-md rounded-[10px] text-[#6155F5] h-6"
 					size="sm"
 					variant="default"
 				>
