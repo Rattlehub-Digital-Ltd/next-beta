7b3977970896b90d478323f99c5062c2d2e44215
Themba <mndebele.themba@gmail.com>
2025-08-08 01:10:54 +0200

    up

--- src/features/dashboard/banner.tsx
+++ src/features/dashboard/banner.tsx
@@ -14,7 +14,7 @@ export default function Banner() {
 		<div className="h-10 flex items-center w-full bg-[#6155F5] text-white pl-4 pr-2 space-x-2  rounded-[16px]">
 			<SparkleIcon className="h-4 w-4 shrink-0" />
 			<p className="font-medium grow truncate text-[12px] tracking-wide">
-				Your Trial ends in{" "}
+				Your trial ends in{" "}
 				<span className="text-yellow-300 font-bold">10 days</span>
 			</p>
 			<StripeDrawer>
