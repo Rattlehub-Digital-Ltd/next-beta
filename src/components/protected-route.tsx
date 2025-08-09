9206ba400818be2cea15ab52e3f162c9f30df201
Themba <mndebele.themba@gmail.com>
2025-08-09 22:46:21 +0200

    up

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -6,76 +6,33 @@ import {
 	type PropsWithChildren,
 	useCallback,
 	useEffect,
-	useRef,
 	useState,
 } from "react";
-import { toast } from "sonner";
 import { useActivitySummaryStore } from "store/use-activity-summary-store";
 import { useAppStore } from "store/use-app-store";
 import { useOnboardingStore } from "store/use-onboarding-store";
-import type { OnboardingStatus } from "@/api/services/dashboard/onboarding/types";
 import Loading from "@/features/shared/loading";
-import useApi from "@/hooks/use-api";
 
 export default function ProtectedRoute({ children }: PropsWithChildren) {
 	const { isAuthenticated } = useAuth0();
-	const { getOnboardingStatus, getActivitySummary } = useApi();
 
-	const { setIsOnboarded, isOnboarded } = useOnboardingStore();
-	const { activity, setActivity } = useActivitySummaryStore();
-
-	const { setInitialized, initialized } = useAppStore();
+	const { isOnboarded } = useOnboardingStore();
+	const { activity } = useActivitySummaryStore();
+	const { initialized } = useAppStore();
 
 	const [isLoading, setIsLoading] = useState(false);
-
-	const onboardingChecked = useRef(
-		isOnboarded !== null && isOnboarded !== undefined,
-	);
-	const activityChecked = useRef(activity !== null && activity !== undefined);
+	const [error, setError] = useState(false);
 
 	const checkInitialization = useCallback(async () => {
 		if (!isAuthenticated) redirect("/login");
 		if (initialized) return;
 
-		try {
-			setIsLoading(true);
+		setIsLoading(true);
 
-			let onboardingStatusResp: OnboardingStatus | undefined;
-			let isOnboardedValue = false;
+		if (isOnboarded && activity) setError(true);
 
-			if (!onboardingChecked.current) {
-				onboardingStatusResp = await getOnboardingStatus();
-				if (onboardingStatusResp?.data) {
-					isOnboardedValue = onboardingStatusResp.data.isOnboarded;
-					setIsOnboarded(isOnboardedValue);
-				}
-			}
-
-			if (!activityChecked.current) {
-				const activitySummaryResp = await getActivitySummary();
-
-				if (activitySummaryResp?.data) {
-					setActivity(activitySummaryResp.data);
-					setInitialized(true);
-				}
-			}
-
-			setIsLoading(false);
-		} catch (error) {
-			console.log(error);
-			setIsLoading(false);
-			setInitialized(false);
-			toast.error("Error: Private Route, Something went wrong");
-		}
-	}, [
-		initialized,
-		setActivity,
-		setIsOnboarded,
-		getActivitySummary,
-		getOnboardingStatus,
-		isAuthenticated,
-		setInitialized,
-	]);
+		setIsLoading(false);
+	}, [initialized, isAuthenticated, isOnboarded, activity]);
 
 	useEffect(() => {
 		checkInitialization();
@@ -85,8 +42,7 @@ export default function ProtectedRoute({ children }: PropsWithChildren) {
 
 	if (!isAuthenticated) redirect("/login");
 
-	if (!isLoading && !initialized)
-		return <div className="p-4">Private route: Access Forbidden!</div>;
+	if (error) return <div className="p-4">Unauthorized: Access Forbidden!</div>;
 
 	return <div className="h-full w-full">{children}</div>;
 }
