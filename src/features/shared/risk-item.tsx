591408078ec59ba16f853eed10a105e6b99e61e3
Themba <mndebele.themba@gmail.com>
2025-08-28 12:16:30 +0200

    style: adjust opacity of eduText paragraph for improved readability

--- src/features/shared/risk-item.tsx
+++ src/features/shared/risk-item.tsx
@@ -15,7 +15,7 @@ export default function RiskItem({
 		<div className="space-y-3 p-3 rounded-[12px] border bg-indigo-50/30 border-indigo-100 backdrop-blur-[25px] shadow-[0px_12px_30px_0px rgba(106, 106, 106, 0.06)]">
 			<h1 className="text-xs font-medium leading-4 pl-0.5">{goalName}</h1>
 			<CategoryList items={[category]} />
-			<p className="text-[0.78rem] opacity-85 text-pretty">{eduText}</p>
+			<p className="text-[0.78rem] opacity-75 text-pretty">{eduText}</p>
 		</div>
 	);
 }
