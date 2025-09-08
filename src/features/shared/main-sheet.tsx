b9591f95ac959924c55965580168daffd44f45e8
Themba <mndebele.themba@gmail.com>
2025-09-08 12:55:34 +0200

    Improve LCP by marking main sheet image as high priority
    Add the `priority` prop to the `Image` component in `MainSheet` to ensure it is preloaded, optimizing the Largest Contentful Paint (LCP) metric.

--- src/features/shared/main-sheet.tsx
+++ src/features/shared/main-sheet.tsx
@@ -35,6 +35,7 @@ function MainSheet({ title, imgSrc, imgAlt, content }: MainSheetProps) {
 						alt={imgAlt ?? "Image"}
 						height={192}
 						width={192}
+						priority
 						loading="eager"
 					/>
 				</div>
