cad5ae8836c8e77c544acb168416d18b2a01aecd
Themba <mndebele.themba@gmail.com>
2025-09-19 20:04:35 +0200

    feat(dashboard): Dynamically display family member count on overview card
    The family member count on the dashboard's overview card was previously hardcoded to '5'. This commit updates the description to dynamically reflect the actual number of family members based on the `items` array length. If no items are present, it will display '0 family members'. This ensures the displayed information is accurate and up-to-date.

--- src/features/dashboard/overview/cards/family.tsx
+++ src/features/dashboard/overview/cards/family.tsx
@@ -30,7 +30,9 @@ export default function Family() {
 				<div className="w-full h-full bg-[#FFFFFF]/65 p-4 border border-[#EBEDED] rounded-3xl backdrop-blur-[60px] shadow-[0px_12px_30px_0px rgba(106, 106, 106, 0.06)] space-y-8 flex flex-col">
 					<SuggestionItem
 						title="Family"
-						description={`5 family members`}
+						description={
+							items ? `${items?.length} family members` : "0 family members"
+						}
 						showReminder={false}
 						color="indigo"
 					/>
