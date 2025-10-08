18e4558ba7cd970db97f93fda978087c3f57d7fd
Themba <mndebele.themba@gmail.com>
2025-10-08 17:00:20 +0200

    refactor: Remove unused integrity attribute from adaptive cards script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
 				<Script
 					strategy="afterInteractive"
 					src="https://unpkg.com/adaptivecards@3.0.4/dist/adaptivecards.min.js"
-					// integrity="sha384-JcibXe4w3Ha9x1e9G+nM3mQp3AgjWsS0MpWUftukzLqUJbEurvYZHPfE284wljCZ"
 					crossOrigin="anonymous"
 				/>
 			</head>
